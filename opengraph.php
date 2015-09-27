93878b58e201722ee6b8d40582c06b4f40c2623a
simajzenman <simajzenman@users.noreply.github.com>
2015-09-27 21:52:50 +0200

    Update opengraph.php

--- opengraph.php
+++ opengraph.php
@@ -1,14 +1,13 @@
 
- 	
-		
  		<meta property="og:title" content="Cercle des étudiant de l' ESIAJ">
 		<meta property="og:site_name" content="ESIAJ FORMULAIRE">
-		<meta property="og:image" content="http://simon-ajzenman.be/php/remise2.jpg">
+		<meta property="og:image" content="http://www.simon-ajzenman.be/php-remise2/img/logo_og.jpg">
 		<meta property="og:image:width" content="861">
 		<meta property="og:image:height" content="380">
-		<meta property="og:url" content="http://simon-ajzenman.be/cercleesiaj">
+		<meta name="twitter:image:src" content="http://www.simon-ajzenman.be/php-remise2/img/logo_og.jpg">
+		<meta property="og:url" content="http://www.simon-ajzenman.be/php-remise2/">
 		<meta property="og:description" content="Formulaire servant à évaluer les raisons des étudiants de vouloir faire leur baptême">
 		<meta property="fb:app_id" content="Formulaire de l ESIAJ">
 		<meta property="og:type" content="website"> 
-
-
+		<meta name="twitter:card" content="product">
+		<meta name="twitter:site" content="sim2629">
